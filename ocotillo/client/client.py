745ccef674448b5914a34fef56f5254eed77011f
Zack Feldstein <zack.feldstein@blackboard.com>
2018-09-02 03:04:37 -0500

    working cli search

--- ocotillo/client/client.py
+++ ocotillo/client/client.py
@@ -1,19 +1,20 @@
+#!/usr/bin/env python
 import requests
 import argparse
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("-e", "--endpoint", action="store",
-#                     help="Person place or thing you want data about.")
-# parser.add_argument()
-# args = parser.parse_args()
-# 
-# if not args.endpoint or args.endpoint not in ['facts']:
-#     exit('no endpoint argument specified')
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--search", action="store",
+                    help="the document db to search")
+parser.add_argument("-sb", "--subject", action='store', 
+                    help="Person place or thing you want data about. ( -sb Earth)")
+parser.add_argument("-f", "--facts", action='store', 
+                    help="information relating to the subject (-f oceans) ")
+args = parser.parse_args()
 
-URL = 'http://localhost:5000/api/v1/acct_01/space_doc'
+ACCOUNT = 'acct_01'
+URL = 'http://localhost:5000/api/v1/%s/%s' % (ACCOUNT, args.search)
 
 HEADERS = {'content-type': 'application/json'}
-payload = {"subject": "Mars", 'object': 'Sun'}
-# payload = {"subject": "Mars"}
+payload = {"subject": args.subject, 'object': args.facts}
 r = requests.get(URL, headers=HEADERS, json=payload)
 print(r.text)
